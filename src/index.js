3b8ce472ec01b071388d714de705d00bf2b3756c
nelotgrom <nelotgrom@gmail.com>
2020-03-07 14:38:56 +0300

    files: download index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let a = Math.abs(n)
+        .toString(10)
+        .split("")
+        .reverse()
+        .join("");
+    return +a;
+};
